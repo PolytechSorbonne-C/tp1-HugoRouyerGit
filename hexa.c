3b634ff58bfcf62467f4c6e9c332112e36bf9a4a
HugoRouyerGit <71442447+HugoRouyerGit@users.noreply.github.com>
2020-09-29 10:52:42 +0200

    Corrigé exo hexa

--- hexa.c
+++ hexa.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int decimal,valide,puissance;
+    int decimal,valide,val;
     char hexa;
 
-    valide = 1;
-    decimal=-1;
-    puissance=0;
-
+    valide=1;
+    decimal=0;
     while (valide){
-        printf("Saisie du prochain caractere :");
-        scanf("%c",&hexa);
-        getchar();
-        switch (hexa){
-            case '0' : decimal+=0; break;
-            case '1' : decimal+=1*pow(16,puissance); break;
-            case '2' : decimal+=2*pow(16,puissance); break;
-            case '3' : decimal+=3*pow(16,puissance); break;
-            case '4' : decimal+=4*pow(16,puissance); break;
-            case '5' : decimal+=5*pow(16,puissance); break;
-            case '6' : decimal+=6*pow(16,puissance); break;
-            case '7' : decimal+=7*pow(16,puissance); break;
-            case '8' : decimal+=8*pow(16,puissance); break;
-            case '9' : decimal+=9*pow(16,puissance); break;
-            case 'A' : decimal+=10*pow(16,puissance); break;
-            case 'B' : decimal+=11*pow(16,puissance); break;
-            case 'C' : decimal+=12*pow(16,puissance); break;
-            case 'D' : decimal+=13*pow(16,puissance); break;
-            case 'E' : decimal+=14*pow(16,puissance); break;
-            case 'F' : decimal+=15*pow(16,puissance);
-            default: valide=0;
-        }
-        puissance+=1;
+    printf("Saisie du prochain caractere :");
+    scanf("%c",&hexa);
+    getchar();
+    if (hexa>='a' && hexa<='f'){
+        val = hexa-'a'+10;
+        decimal = decimal*16 + val;
     }
-
-    if (decimal!=-1)
-        printf("Valeur décimale : %d",decimal+1);
+    else if (hexa>='A' && hexa<='F'){
+        val = hexa-'A'+10;
+        decimal = decimal*16 + val;
+    }
+    else if (hexa>='0' && hexa<='9'){
+        val = hexa-'9'+9;
+        decimal = decimal*16 + val;
+    }
     else
-        printf("Nombre Hexa non valide");
+        valide=0;
+
+
+    }
+    printf("%d",decimal);
+
+
 
     return 0;
 }
